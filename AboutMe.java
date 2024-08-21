6123db85c292793680254243e7bd4c681f821c6e
charlieklop <charlieklop@gmail.com>
2024-08-21 09:15:35 -0500

    AboutMe

---
+++ AboutMe.java
@@ -0,0 +1,8 @@
+public class AboutMe {
+    public static void main(String[] args){
+        System.out.println("I ate pie for breakfast");
+        System.out.println("I like video games");
+        System.out.println("I should have bought bitcoin in 2013 instead of being 7 years old.");
+
+    }
+}
